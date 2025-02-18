753887f41d8989803a1320b07191beb466e29f3b
Ben Fortuna <fortuna@nodelogic.au>
2025-02-19 10:06:26 +1100

    License update

--- src/main/java/au/nodelogic/coucal/workspaces/controller/AbstractLayoutController.java
+++ src/main/java/au/nodelogic/coucal/workspaces/controller/AbstractLayoutController.java
@@ -1,8 +1,24 @@
+/*
+ *  Copyright 2025 Node Logic
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package au.nodelogic.coucal.workspaces.controller;
 
-import au.nodelogic.coucal.workspaces.data.CollectionManager;
+import au.nodelogic.coucal.workspaces.CollectionManager;
+import au.nodelogic.coucal.workspaces.EntityManager;
 import au.nodelogic.coucal.workspaces.data.EmailMessageRepository;
-import au.nodelogic.coucal.workspaces.data.EntityManager;
 import au.nodelogic.coucal.workspaces.data.FeedItemRepository;
 import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/test/groovy/au/nodelogic/coucal/workspaces/channel/FeedServiceTest.groovy
+++ src/test/groovy/au/nodelogic/coucal/workspaces/channel/FeedServiceTest.groovy
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2025 Node Logic
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package au.nodelogic.coucal.workspaces.channel
 
 import spock.lang.Specification
@@ -21,5 +37,6 @@ class FeedServiceTest extends Specification {
                                                    'https://www.cloudflarestatus.com/history.rss',
                                                    'https://www.cloudflarestatus.com/history.atom']
         'https://www.linkedin.com/in/benfortuna/' | []
+        'https://news.google.com/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKQlZTZ0FQAQ?hl=en-AU&gl=AU&ceid=AU%3Aen' | []
     }
 }
