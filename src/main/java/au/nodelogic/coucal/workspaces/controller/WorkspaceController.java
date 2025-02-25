c4f22e7da8d0699c6802a22a56f00673a779e029
Ben Fortuna <fortuna@nodelogic.au>
2025-02-26 09:49:13 +1100

    Added controller skeletons

---
+++ src/main/java/au/nodelogic/coucal/workspaces/controller/WorkspaceController.java
@@ -0,0 +1,9 @@
+package au.nodelogic.coucal.workspaces.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/workspaces")
+public class WorkspaceController {
+}
