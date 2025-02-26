312bbaab5e8d5999d1572f2c7975fab2ae24dbe4
Ben Fortuna <ben.fortuna@team.telstra.com>
2025-02-26 17:09:56 +1100

    UI enhancements

---
+++ src/main/java/au/nodelogic/coucal/workspaces/controller/EntityController.java
@@ -0,0 +1,28 @@
+package au.nodelogic.coucal.workspaces.controller;
+
+import au.nodelogic.coucal.workspaces.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/entities")
+public class EntityController {
+
+    private final EntityManager entityManager;
+
+    public EntityController(@Autowired EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @GetMapping("/{kind}")
+    public String listEntities(@PathVariable(name = "kind") String kind, Model model) throws IOException {
+        model.addAttribute("entities", entityManager.getEntityCollection().getAll());
+        return "entities/list";
+    }
+}
--- src/main/java/au/nodelogic/coucal/workspaces/controller/WorkspaceController.java
+++ src/main/java/au/nodelogic/coucal/workspaces/controller/WorkspaceController.java
@@ -1,9 +1,19 @@
 package au.nodelogic.coucal.workspaces.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/workspaces")
 public class WorkspaceController {
+
+    @GetMapping("/recent")
+    public String listRecent(Model model) throws IOException {
+        return "workspaces/recent";
+    }
 }
